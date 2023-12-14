d77c1a83d5af7af97924eaf8b3d3f5e31cabcf3e
Manuel Flores <jmfh.developer@outlook.com>
2023-12-14 12:56:42 -0600

    rediseño

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -19,7 +19,7 @@
 
     <div class="py-12">
         <div class="max-w-full mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-3xl">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-3xl shadow-red-600/30">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="flex flex-col text-center w-full mb-10">
                         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Clientes registrados</h1>
--- resources/views/pagarcuenta/index.blade.php
+++ resources/views/pagarcuenta/index.blade.php
@@ -19,7 +19,7 @@
 
     <div class="py-12">
         <div class="max-full sm:px-6 ">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-3xl">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-3xl shadow-red-600/30">
                 <div class="block rounded-3xl bg-white text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
 
                     <div class="p-6">
